0c09df0096b1575ce7faeeb5b8550fda988934f1
Dumo-B <deebozongwana@yahoo.com>
2023-02-20 15:52:14 -0800

    5 Update 1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include<stdio.h>
+
+/**
+* main - prints the size of various types on the computer it is compiled and run on
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
