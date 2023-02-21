f6d7f3df8c79d907fbb6f45164137c54f6ce3028
Dumo-B <deebozongwana@yahoo.com>
2023-02-20 17:43:14 -0800

    Update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lower case
+ *
+ * Return: (0) Success
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
