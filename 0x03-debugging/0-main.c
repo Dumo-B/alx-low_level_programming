5f28b0544bc97395a3486b9ea685267332d285ce
Dumo-B <deebozongwana@yahoo.com>
2023-02-24 14:54:25 -0800

    1 Update 2

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#INCLUDE "main.h"
+
+/**
+ * main - tests function that prints if value is positive or negative
+ *
+ * Return: (o)
+ */
+
+in main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
