a6eebacacf2234b4de2e898757fb913eea86cac9
Dumo-B <deebozongwana@yahoo.com>
2023-03-09 13:21:22 -0800

    Task 4 v2

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - returns the value of x raised to the power of y
  *  @x: given value
  *  @y: to the power of this value
- *  
+ *
  *  Return: (-1)
  */
 
@@ -14,7 +14,7 @@ int _pow_recursion(int x, int y)
 {
 	return (-1);
 }
-	else if (y != 0)
+else if (y != 0)
 return (x * _pow_recursion(x, y - 1));
 else
 return (1);
