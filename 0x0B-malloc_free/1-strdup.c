cf9d8948e868aef76a1933a16a5aad0b5a8ea502
Dumo-B <deebozongwana@yahoo.com>
2023-03-14 15:07:29 -0700

    Task 0 and Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strdup - string duplicator function
+ * @str: string to be duplicated
+ * Return: a string pointer
+ */
+char *_strdup(char *str)
+{
+	int a = 1, b = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[a])
+		a++;
+	s = (char *) malloc(a * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+	while (b < a)
+	{
+		s[j] = str[b];
+		b++;
+	}
+	s[b] = '\0';
+	return (s);
+}
